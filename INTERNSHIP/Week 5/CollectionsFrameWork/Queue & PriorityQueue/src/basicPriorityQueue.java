eadb11c6a7d9e25a5ac940f17cd5214be1d5a27f
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2022-05-17 15:15:41 +0530

    updated

--- INTERNSHIP/Week 5/CollectionsFrameWork/Queue & PriorityQueue/src/basicPriorityQueue.java
+++ INTERNSHIP/Week 5/CollectionsFrameWork/Queue & PriorityQueue/src/basicPriorityQueue.java
@@ -1,5 +1,33 @@
+import java.util.*; 
+
 public class basicPriorityQueue {
-    public static void main(String[] args) {
-        
-    }
-}
+
+	public static void main(String args[]){  
+		
+		PriorityQueue<String> queue=new PriorityQueue<String>();  
+		queue.add("Sandeep");  
+		queue.add("Vijay");  
+		queue.add("JaiShankar");  
+		queue.add("Raj");  
+
+		System.out.println("head:"+queue.element());  
+		System.out.println("head:"+queue.peek());  
+
+		System.out.println("\nIterating the queue elements:");  
+
+		Iterator itr=queue.iterator();  
+		while(itr.hasNext()){  
+			System.out.println(itr.next());  
+		}  
+		
+		queue.remove();  
+		queue.poll();  
+		
+		System.out.println("\nAfter removing two elements:");  
+		
+		Iterator<String> itr2=queue.iterator();  
+		while(itr2.hasNext()){  
+			System.out.println(itr2.next());  
+		}  
+	}  
+} 
