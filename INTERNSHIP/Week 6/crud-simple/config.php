34a81cef2dfe1ca5ad539c85d08fcfea2d2e9cca
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2022-05-31 14:56:46 +0530

    CRUD OPERATION USING PHP & MYSQL

---
+++ INTERNSHIP/Week 6/crud-simple/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+//Connecting to the Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, "phpcrud");
+
+?>
